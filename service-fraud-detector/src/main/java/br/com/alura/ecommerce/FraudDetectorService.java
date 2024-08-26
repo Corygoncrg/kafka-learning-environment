d6883c5047a06a09967700ebb40d9c6417cec06a
Rodrigo Hajer <rodrigohajer11@hotmail.com>
2024-08-26 13:42:12 -0300

    Initial commit

---
+++ service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -0,0 +1,55 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class FraudDetectorService {
+    public static void main(String[] args) {
+        var fraudService = new FraudDetectorService();
+        try (var service = new KafkaService<>(FraudDetectorService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER", fraudService::parse, Order.class, Map.of())){
+        service.run();
+        }
+    }
+    private void parse(ConsumerRecord<String, Order> record) throws ExecutionException, InterruptedException {
+        System.out.println("---------------------------");
+        System.out.println("Processing new order, checking for fraud");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // ignoring
+            e.printStackTrace();
+        }
+        var order = record.value();
+        if (isFraud(order)){
+            // fraud happening
+            System.out.println("Order fraud: " + order);
+            orderDispatcher.send("ECOMMERCE_ORDER_REJECTED", order.getEmail(), order);
+        } else {
+            // only send approval if not fraud
+            System.out.println("Order approved: " + order);
+            orderDispatcher.send("ECOMMERCE_ORDER_APPROVED", order.getEmail(), order);
+        }
+    }
+
+    private static boolean isFraud(Order order) {
+        return order.getAmount().compareTo(new BigDecimal(4500)) >= 0;
+    }
+
+    private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+        return properties;
+    }
+}
---
+++ service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -0,0 +1,26 @@
+package br.com.alura.ecommerce;
+
+
+import java.math.BigDecimal;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailDispatcher = new KafkaDispatcher<String>()) {
+                for (var i = 0; i < 10; i++) {
+                    var email = Math.random() + "@email.com";
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = BigDecimal.valueOf(Math.random() * 5000 + 1);
+                    var order = new Order(orderId, amount, email);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", email, order);
+
+                    var emailCode = "Thank you for your order! We are now processing it!";
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email, emailCode);
+                }
+            }
+        }
+    }
+}
